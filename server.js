f3f23727ea088a45fcd015e7075448e6407b25af
Vivek Srivastava <viveksrivastava6180@gmail.com>
2021-11-20 21:08:26 +0530

    remove comment

--- server.js
+++ server.js
@@ -21,17 +21,6 @@ const client = new Client({
   }
 });
 
-// const client = new Client({
-//   user: "hkhpdsqnzogfvt",
-//   host: "ec2-52-208-185-143.eu-west-1.compute.amazonaws.com",
-//   database: "dd14r915oum6b7",
-//   password: "fa1b1e6ce7c619881d03c3fc20db6e4eedb2f392455eb1e279b9f793f5da0fb7",
-//   port: 5432,
-// 	ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
 client.connect((...a)=>{
 	console.log(a)
 	console.log('Database conected');
