0d5dee15c2efb946724bdb3e4ff3abd9a2aeba47
Vivek Srivastava <viveksrivastava6180@gmail.com>
2021-11-20 21:06:57 +0530

    added DB

--- server.js
+++ server.js
@@ -15,24 +15,39 @@ const client = new Client({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+	ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-client.connect(()=>{
+// const client = new Client({
+//   user: "hkhpdsqnzogfvt",
+//   host: "ec2-52-208-185-143.eu-west-1.compute.amazonaws.com",
+//   database: "dd14r915oum6b7",
+//   password: "fa1b1e6ce7c619881d03c3fc20db6e4eedb2f392455eb1e279b9f793f5da0fb7",
+//   port: 5432,
+// 	ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+client.connect((...a)=>{
+	console.log(a)
 	console.log('Database conected');
 });
 
-app.post('/keyword',(req,res)=>{
-	var search = req.body.search.trim().toLowerCase();
-	var pageNum = parseInt(req.body.pageNum) || 1;
-	var limit = `limit 30 offset ${30*(pageNum-1)}`;
+app.get('/keyword',(req,res)=>{
+	var search = req.query.search.trim().toLowerCase();
+	var pageNum = parseInt(req?.query?.page) || 1;
+	var limit = req.query.page?`limit 30 offset ${30*(pageNum-1)}`:"";
 	
 	var q = search.split(" ").reduce((acc,val)=>{
 		return acc+` or keyword like '%${val}%'`;
 	},'').replace(' or','');;
 
-	q=`select 1 as exact,keyword,country from keyword where keyword like '%${search}%' union select 0 as exact,keyword,country from keyword where keyword not like '%${search}%' and (${q}) order by exact desc ${limit}`;
-	// console.log(q);
+	q=`select TRUE as exact,keyword,country from keyword where keyword like '%${search}%' union select FALSE as exact,keyword,country from keyword where keyword not like '%${search}%' and (${q}) order by exact desc ${limit}`;
+	 console.log(q);
 	client.query(q,(err,results)=>{
 		res.json({ pageNum, data: results.rows });
 	});
